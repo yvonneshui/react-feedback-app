0b987315b54d93c29b90d70ac608a4fb12e74782
yvonneshui <yvonne.shui126@gmail.com>
2022-02-04 14:20:44 -0500

    edit feedback feature added

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -5,16 +5,28 @@ const FeedbackContext = createContext();
 
 export const FeedbackProvider = ({ children }) => {
   const [feedback, setFeedback] = useState(FeedbackData);
-
+  const [feedbackEdit, setFeedbackEdit] = useState({
+    item: {},
+    edit: false,
+  });
+  //delete feedback
   const deleteFeedback = (id) => {
     if (window.confirm("Are you sure you want to delete this?")) {
       setFeedback(feedback.filter((item) => item.id !== id));
     }
   };
-
+  //Add feedback
   const addFeedback = (newFeedback) => {
     newFeedback.id = uuidv4();
     setFeedback([newFeedback, ...feedback]);
+  };
+
+  //Set item to be updated
+  const editFeedback = (item) => {
+    setFeedbackEdit({
+      item,
+      edit: true,
+    });
   };
 
   return (
@@ -23,6 +35,7 @@ export const FeedbackProvider = ({ children }) => {
         feedback,
         deleteFeedback,
         addFeedback,
+        editFeedback,
       }}
     >
       {children}

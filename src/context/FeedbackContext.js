44b49d5a1ae83631bb6a8da360b817a2673cfea4
yvonneshui <yvonne.shui126@gmail.com>
2022-02-14 11:34:12 -0500

    feature:update from json server

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -49,9 +49,17 @@ export const FeedbackProvider = ({ children }) => {
     });
   };
   //Update feedback item
-  const updateFeedback = (id, updItem) => {
+  const updateFeedback = async (id, updItem) => {
+    const response = await fetch(`/feedback/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(updItem),
+    });
+    const data = await response.json();
     setFeedback(
-      feedback.map((item) => (item.id === id ? { ...item, ...updItem } : item))
+      feedback.map((item) => (item.id === id ? { ...item, ...data } : item))
     );
   };
 
